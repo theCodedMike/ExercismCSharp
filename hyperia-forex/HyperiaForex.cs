ff1fa1dc0dfcbe3e04e5a38eeee637c7bd06544e
lxf <lixiaofeng1115@outlook.com>
2024-10-29 15:00:17 +0800

    28: hyperia-forex(表达式版)

--- hyperia-forex/HyperiaForex.cs
+++ hyperia-forex/HyperiaForex.cs
@@ -1,4 +1,5 @@
 using Xunit.Sdk;
+using System;
 
 public struct CurrencyAmount
 {
@@ -10,75 +11,40 @@ public struct CurrencyAmount
         this.amount = amount;
         this.currency = currency;
     }
-    
-    // implement equality operators
-    public static bool operator ==(CurrencyAmount x, CurrencyAmount y)
-    {
-        if (x.currency != y.currency)
-        {
-            throw new ArgumentException();
-        }
 
-        return x.amount == y.amount;
-    }
-    public static bool operator !=(CurrencyAmount x, CurrencyAmount y)
-    {
-        return !(x == y);
-    }
+    private static bool ValidateCurrency(CurrencyAmount x, CurrencyAmount y) => x.currency == y.currency;
+
+    // implement equality operators
+    public static bool operator ==(CurrencyAmount x, CurrencyAmount y) =>
+        ValidateCurrency(x, y) ? x.amount == y.amount : throw new ArgumentException();
+
+    public static bool operator !=(CurrencyAmount x, CurrencyAmount y) => !(x == y);
 
     // implement comparison operators
-    public static bool operator >(CurrencyAmount x, CurrencyAmount y)
-    {
-        if (x.currency != y.currency)
-        {
-            throw new ArgumentException();
-        }
+    public static bool operator >(CurrencyAmount x, CurrencyAmount y) =>
+        ValidateCurrency(x, y) ? x.amount > y.amount : throw new ArgumentException();
 
-        return x.amount > y.amount;
-    }
-    public static bool operator <(CurrencyAmount x, CurrencyAmount y)
-    {
-        return !(x > y);
-    }
+    public static bool operator <(CurrencyAmount x, CurrencyAmount y) => !(x > y);
 
     // implement arithmetic operators
-    public static CurrencyAmount operator +(CurrencyAmount x, CurrencyAmount y)
-    {
-        if (x.currency != y.currency)
-        {
-            throw new ArgumentException();
-        }
+    public static CurrencyAmount operator +(CurrencyAmount x, CurrencyAmount y) => ValidateCurrency(x, y)
+        ? new CurrencyAmount(x.amount + y.amount, x.currency)
+        : throw new ArgumentException();
 
-        return new CurrencyAmount(x.amount + y.amount, x.currency);
-    }
-    
-    public static CurrencyAmount operator -(CurrencyAmount x, CurrencyAmount y)
-    {
-        if (x.currency != y.currency)
-        {
-            throw new ArgumentException();
-        }
+    public static CurrencyAmount operator -(CurrencyAmount x, CurrencyAmount y) => ValidateCurrency(x, y)
+        ? new CurrencyAmount(x.amount - y.amount, x.currency)
+        : throw new ArgumentException();
 
-        return new CurrencyAmount(x.amount - y.amount, x.currency);
-    }
-    
-    public static CurrencyAmount operator *(CurrencyAmount x, decimal y)
-    {
-        return new CurrencyAmount(x.amount * y, x.currency);
-    }
-    
+    public static CurrencyAmount operator *(CurrencyAmount x, decimal y) => new(x.amount * y, x.currency);
+
     public static CurrencyAmount operator /(CurrencyAmount x, decimal y)
     {
+        if (y == 0) throw new DivideByZeroException();
+
         return new CurrencyAmount(x.amount / y, x.currency);
     }
 
     // implement type conversion operators
-    public static explicit operator double(CurrencyAmount x)
-    {
-        return (double)x.amount;
-    }
-    public static implicit operator decimal(CurrencyAmount x)
-    {
-        return x.amount;
-    }
+    public static explicit operator double(CurrencyAmount x) => (double)x.amount;
+    public static implicit operator decimal(CurrencyAmount x) => x.amount;
 }
